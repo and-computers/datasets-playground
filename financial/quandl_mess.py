cedb602856e146762235ef01c05115d5c207b724
tosin <tosin.sonuyi@gmail.com>
2021-01-12 22:27:43 -0500

    add idea skeletons for quandl and tabula

---
+++ financial/quandl_mess.py
@@ -0,0 +1,7 @@
+import quandl
+
+hydro_production_in_ng = quandl.get("UENG/EH_PROD_NGA")
+avg_canada_unabsorbed_homeowner = quandl.get("CMHC/HPPU50_BC")
+tsla_short = quandl.get("FINRA/FNRA_TSLA")
+
+
---
+++ great_migration/uscensus.py
@@ -0,0 +1,3 @@
+import tabula
+
+# read census data on US internal migration 1910-1960
